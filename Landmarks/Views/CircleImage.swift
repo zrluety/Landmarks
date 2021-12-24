2c80d52df280de0c1e810c9e5839a7bd3f0b1c57
Zachary Luety <zrluety216@gmail.com>
2021-12-24 13:09:18 -0700

    Competed Tutorial Part 1

---
+++ Landmarks/Views/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Zachary Luety on 12/24/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
